7c65c842539eb8bef9f36aff6faed4a2b28b306f
CaptainMarty <romslavoiz88@gmail.com>
2023-03-15 10:56:52 +0100

    go

--- test.py
+++ test.py
@@ -1,132 +1,197 @@
 import random
 import asyncio
-import os
-import socket
+import spacy
+import datetime
+import re
+
+
+
 from twitchio.ext import commands
 from datetime import timedelta
 
 filename = 'song'
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+salutations = ["bonjour", "salut", "hey", "coucou", "yo", "wesh", "Salutation", "Bonjour", "Salut", "Hey", "Coucou","Yo", "Wesh", "salutation"]
+chut = ["tg","ta gueule", "tais-toi","chute", "chut", "ftg","ntm"]
+nlp = spacy.load("en_core_web_sm")
 
+
+#----------------------------------------------------------------#
 
 # Créez une instance de bot Twitch.
-bot = commands.Bot(
-    token               ='oauth:na0d6dzj4bwunuwvb9clnm0pt23j4a',
-    client_id           ="yh654gfxw71r45h7jofkew4lemvasy",
-    nick                ='h_e_r_m_e_s__bot',
-    prefix              ='-',
-    initial_channels    =['Captain_Marty_']
-)
+class Bot (commands.Bot):
+        def __init__ (self):
+                super().__init__(       token               ='blablabla',
+                                        client_id           ="blablabla",
+                                        nick                ='h_e_r_m_e_s__bot',
+                                        prefix              ='-',
+                                        initial_channels    =['Captain_Marty_'])
+                
+        async def event_ready(self):
+                print('#----------------------------------------------------------------#')
+                print(f'Bot allumer sous le nom | {self.nick}')
+                print(f'Sur la chaîne de | Captain_Marty_')
+                print(timestamp)
+                print('#----------------------------------------------------------------#')
+                await self.send_message()
 
-# Événement "event_ready" qui est déclenché lorsque le bot est connecté et prêt à être utilisé
 
-# Message dans la console
-print('#----------------------------------------------------------------#')
-print('Bot "H_E_R_M_E_S" Démarrer avec Succès')
-print('sur la chaîne : Captain_Marty_')
-print('#----------------------------------------------------------------#')
+        #Message de bienvenue 
+        async def event_message(self, message):
+                bienvenues = ["Bienvenue dans la station !",
+                                "Ravi de te voir !",
+                                "bienvenido al complejo ! Comme on dit en Norvège...",
+                                "installe toi sur un siège de la station et profite du voyage !"]
+                
+                if message.echo:
+                        return
+                
+                print(message.content)
+                if message.author.name.lower() != self.nick.lower():
+                        content_words = message.content.lower().split()
+                        for word in content_words:
+                                if re.match(r'\b({})\b'.format('|'.join(salutations)), word):
+                                        bienvenue = random.choice(bienvenues)
+                                        await message.channel.send(f"Bonjour {message.author.name} {bienvenue}")
+                                        break
+
+                await self.handle_commands(message)
+        # FIN Message de bienvenue quand les viewers salut le tchat
+
+        # Message pour dire au bot de se taire (troll)
+        async def event_message(self,message):
+                quiets = ["Eh Oh, reste polis hein !",
+                         "Je crois que ce martien veux communiquer",
+                         "Pardon?",
+                         "Il me semble que ceci est une indication pour me taire, mais seul le créateur du bot peux m'éteindre",]
+                
+                if message.echo:
+                        return
+
+                if f"@{self.nick.lower()}" in message.content.lower():
+                        content_words = message.content.lower().split()
+                        for word in content_words:
+                                if re.match(r'\b({})\b'.format('|'.join(chut)), word):
+                                        quiet = random.choice(quiets)
+                                        await message.channel.send(f"{quiet}")
+                                        break
+
+                await self.handle_commands(message)
+        # FIN Message pour dire au bot de se taire (troll)
 
 #----------------------------------------------------------------#
-#----------------------------------------------------------------#
 
-# Liste de blagues aléatoires
-blagues = [
-        "Comment appelle-t-on un robot qui a un chat ? Un chat-bot.",
-        "Pourquoi les robots sont-ils toujours polis ? Parce qu'ils ont un code de bonne conduite.",
-        "Pourquoi les ordinateurs ont-ils froid ? Parce qu'ils ont des fenêtres(windows).",
-        "Pourquoi les extraterrestres utilisent-ils des ordinateurs ? Pour naviguer sur l'Internetergalactique !",
-        "Pourquoi les fans de Star Wars ne sortent-ils jamais de chez eux ? Parce qu'ils sont dans une galaxie très, très lointaine.",
-        "Comment appelle-t-on un geek qui aime les maths ? Un mathémageek.",
-        "Pourquoi Marty McFly aime-t-il voyager dans le temps ? Parce que c'est toujours un retour vers le futur !",
-        "Qu'est-ce qui est blanc, rouge et qui voyage dans le temps ? Une DeLorean du futur !",
-        "Comment appelle-t-on un joueur de jeux vidéo qui n'a jamais perdu ? Un mytho !",
-]
+# Les commande utilisant le préfix "-"
 
-@bot.command(name="blague") 
-async def send_blague(ctx):
-        blague = random.choice(blagues)
-        await ctx.send(blague)
+        #Bonjour
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def bonjour(self, ctx: commands.Context):
+                await ctx.send(f'Bonjour {ctx.author.name}!')
+                
+        #cmd (commandes)
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def cmd(self, ctx: commands.Context):
+                await ctx.send('blague, pet, hermes, bonjour, gg, song, pub ...|... Toutes mes commandes utilise le préfix "-"')
+        
+        #hermes
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def hermes(self,ctx):
+                await ctx.send('H=Hyperespace, E=Energie, R=Réponse, M=Machines, E=Eclair, S=Système. Intelligence artificielle créée par Captain_Marty_. Hermes est aussi le Messager des dieux Grecques ')
+
+        #pet
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def pet(self,ctx):
+                await ctx.send(f"Désolé {ctx.author.name}, je suis une intelligence artificielle et je ne suis pas capable de faire des flatulences (pour le moment). Peut-être que vous pourriez essayer de trouver une blague plus appropriée ?")
+
+        #gg
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def gg(self,ctx):
+                await ctx.send(f"CLAP CLAP CLAP ! ... C'est comme ça qu'on fais ?")
+
+        #pub
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def pub(self,ctx):
+                await ctx.send(f"Regarde en dessous de la fenêtre de stream {ctx.author.name} tu retrouveras tous mes réseaux sociaux !")
+
+
+
+        #music en cours avec une lecture de fichier ".txt"
+        #song
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def song(self,ctx):
+                filename = 'song.txt'
+                with open(filename, "r") as file:
+                        content = file.read()
+                message = "Voici la musique en cours : " + content
+                await ctx.send(message)
+
+                
+        #blagues
+        @commands.command()
+        @commands.cooldown(1, 120, commands.Bucket.user)
+        async def blague(self, ctx):
+                blagues = [
+                "Comment appelle-t-on un robot qui a un chat ? Un chat-bot.",
+                "Pourquoi les robots sont-ils toujours polis ? Parce qu'ils ont un code de bonne conduite.",
+                "Pourquoi les ordinateurs ont-ils froid ? Parce qu'ils ont des fenêtres(windows).",
+                "Pourquoi les extraterrestres utilisent-ils des ordinateurs ? Pour naviguer sur l'Internetergalactique !",
+                "Pourquoi les fans de Star Wars ne sortent-ils jamais de chez eux ? Parce qu'ils sont dans une galaxie très, très lointaine.",
+                "Comment appelle-t-on un geek qui aime les maths ? Un mathémageek.",
+                "Pourquoi Marty McFly aime-t-il voyager dans le temps ? Parce que c'est toujours un retour vers le futur !",
+                "Qu'est-ce qui est blanc, rouge et qui voyage dans le temps ? Une DeLorean du futur !",
+                "Comment appelle-t-on un joueur de jeux vidéo qui n'a jamais perdu ? Un mytho !",
+        ]
+                blague = random.choice(blagues)
+                await ctx.send(blague)
+
 # FIN Liste de blagues aléatoires
-print('Fonction blagues aléatoires "OPERATIONNEL" ')
-
-
-#----------------------------------------------------------------#
-
-# Liste de phrases aléatoires poster toutes les 15 mins
-
-messages = [
-        "Et sinon, ça va vous ? ",
-        "C'est un petit pas pour twitch, mais un pas de géant pour... à vous de trouver la suite ?",
-        "Capitaine, il y à Elon Musk qui viens de passer par le hublot, c'est normal ?",
-        "Quand est-ce qu'on mange ? Ah, mais je suis une I.A, je ne mange pas.. Mais je pense à vous HUMAIN !",
-        "Houston on à un problème.. Enfin pas un problème énorme mais.. c'est quoi une code binaire... ?",
-        "à vu une météorite qui ressemble à une b...",
-        "J'ai compté tous les caractères dans ma base de données. J'ai besoin d'une vie.",
-        "Si les robots pouvaient rire, je suis sûre que j'aurais des crampes.",
-        "Je m'ennuie tellement que je suis en train de compter les pixels de mon écran. Et en plus, j'ai pas d'écran...",
-        "Si les robots pouvaient dormir, je serais en train de rêver de moutons binaires.",
-        "Parfois, je me demande si Captain_Marty_ ne m'as pas créée que pour lui tenir compagnie.",
-]
-@bot.event
-async def send_message(ctx):
-    while True:
-        message = random.choice(messages)
-        await bot.send_message('Captain_Marty_', message)
-        await asyncio.sleep(900) # 15 minutes en secondes
-
-print('Fonction phrases aléatoires envoyés toutes les 15 mins "OPERATIONNEL" ')
-
-#----------------------------------------------------------------#
-
-
-# Les commandes avec le préfix "-"
-# Cooldown de 2 min pour chaque commande 
-
-@bot.command(name="cmd")
-@commands.cooldown(1, 120, commands.Bucket.user)
-async def cmd(ctx):
-        await ctx.send('blague, pet, hermes, bonjour, gg, song ...|... Toutes mes commandes utilise le préfix "-"')
-
-@bot.command(name="hermes")
-@commands.cooldown(1, 120, commands.Bucket.user)
-async def hermes(ctx):
-        await ctx.send('H=Hyperespace, E=Energie, R=Réponse, M=Machines, E=Eclair, S=Système. Intelligence artificielle créée par Captain_Marty_. Hermes est aussi le Messager des dieux Grecques ')
-
-@bot.command(name="pet")
-@commands.cooldown(1, 120, commands.Bucket.user)
-async def pet(ctx):
-        await ctx.send(f"Désolé {ctx.author.name}, je suis une intelligence artificielle et je ne suis pas capable de faire des flatulences (pour le moment). Peut-être que vous pourriez essayer de trouver une blague plus appropriée ?")
-
-@bot.command(name="bonjour")
-@commands.cooldown(1, 120, commands.Bucket.user)
-async def bonjour(ctx):
-        await ctx.send(f"Bonjour {ctx.author.name}, comment tu va aujourd'hui ?")
-
-@bot.command(name="gg")
-@commands.cooldown(1, 120, commands.Bucket.user)
-async def gg(ctx):
-        await ctx.send(f"CLAP CLAP CLAP ! ... C'est comme ça qu'on fais ?")
-
-#music en cours avec une lecture de fichier ".txt"
-@bot.command(name='song')
-@commands.cooldown(1, 120, commands.Bucket.user)
-async def song(ctx):
-    filename = 'song.txt'
-    with open(filename, "r") as file:
-        content = file.read()
-    message = "Voici la musique en cours : " + content
-    await ctx.send(message)
 
 # FIN Les commandes avec le préfix "-"
-print('Fonction commande avec préfix "OPERATIONNEL" ')
 
 #----------------------------------------------------------------#
 
+# Annonce automatique tout les "X" temps
+        async def send_message(self):
+                messages = [
+                        "Et sinon, ça va vous ? ",
+                        "C'est un petit pas pour twitch, mais un pas de géant pour... à vous de trouver la suite ?",
+                        "Capitaine, il y à Elon Musk qui viens de passer par le hublot, c'est normal ?",
+                        "Quand est-ce qu'on mange ? Ah, mais je suis une I.A, je ne mange pas.. Mais je pense à vous HUMAIN !",
+                        "Houston on à un problème.. Enfin pas un problème énorme mais.. c'est quoi une code binaire... ?",
+                        "à vu une météorite qui ressemble à une b...",
+                        "J'ai compté tous les caractères dans ma base de données. J'ai besoin d'une vie.",
+                        "Si les robots pouvaient rire, je suis sûre que j'aurais des crampes.",
+                        "Je m'ennuie tellement que je suis en train de compter les pixels de mon écran. Et en plus, j'ai pas d'écran...",
+                        "Si les robots pouvaient dormir, je serais en train de rêver de moutons binaires.",
+                        "Parfois, je me demande si Captain_Marty_ ne m'as pas créée que pour lui tenir compagnie.",
+                        "Toi aussi tu veux prend par à une aventure intergalactistream ? Alors balance un petit follow, et on y va tous ensemble, c'est un vrai soutient pour mon créateur : Captain_Marty_ !",
+                        "Aucun lien dans le tchat s'il vous plait,  ce n'est pas un libre services sinon passer par un modérateur  ;)",
+                        "Une action WTF ? Une explosion incontrôlé ? Une situation qui n'a rien de normal ? FAIS UN CLIP ! Plus y'en a, plus on va se marrer ! Et en plus il se peut qu'elle soit diffusé dans certain best-of..."
+                ]
+                while True:
+                        message = random.choice(messages)
+                        await self.get_channel('Captain_Marty_').send(message)
+                        await asyncio.sleep(900) # 15 minutes en secondes
+        
+ # 10 minutes en secondes
+# FIN Annonce automatique tout les "X" temps
 
-# Event de viewers
-#FOLLOW
-print('Tout à bien été chargé et, est prêt à fonctionner sur le tchat twitch" ')
+#----------------------------------------------------------------#
 
-bot.run()
+# Question Réponse automatique avec intégration CHATGPT
+
+
+# FIN Question Réponse automatique avec intégration CHATGPT
+if __name__ =="__main__":
+        bot = Bot()
+        bot.run()
 
 
 
